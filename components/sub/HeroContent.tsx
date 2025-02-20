448fc806b88b36822abc307e22b81b269f38aec9
Abhijeet-somkuwar <abhijeetsomkuwar26@gmail.com>
2025-02-21 02:12:58 +0530

    changing names

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -33,21 +33,23 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              {" "}
-              the best{" "}
-            </span>
-            project exprience
+            Crafting  
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">  
+              {" "} scalable solutions {" "}  
+            </span>  
+            with DSA in C++ & full-stack development in Python and other technologies  
           </span>
+
+
+
         </motion.div>
 
         <motion.p
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I&apos;m a skilled Software Engineer proficient in C, C++, Python, HTML, CSS, and experienced in API development, MERN stack, UEFI/BIOS firmware, data science, AI/ML, and containerization. 
+          Explore my projects, including a face recognition attendance system and some websites.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
